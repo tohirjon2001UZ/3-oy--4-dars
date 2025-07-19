68d3a21ea077d50d8f4f776fac39245bb8a87aec
Tohirjon <tohirjonxatamov0924@gmail.com>
2025-07-19 15:51:11 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,254 @@
+// function declaration
+// function expressionda
+// arrow functionda
+
+// 1-misol
+
+// ----------------------------- function declaration ------------------------------------------
+
+// function showAge(age){
+//     console.log(`Sizning yoshingiz: ${age}`);
+
+// }
+// let age=prompt("yoshingizni kiriting: ")
+// showAge(age)
+
+// ----------------------------- function expressionda -----------------------------------------
+
+// const showAge=function(age){
+//     console.log(`Sizning yoshingiz: ${age}`);
+// }
+// let age=prompt("yoshingizni kiriting: ")
+// showAge(age)
+
+// ----------------------------- arrow functionda ----------------------------------------------
+
+// const showAge=()=>{
+//     console.log(`Sizning yoshingiz: ${age}`);
+// }
+// let age=prompt("yoshingizni kiriting: ")
+// showAge(age)
+
+// 2-misol
+
+// ----------------------------- function declaration ------------------------------------------
+// function addNumbers(a,b){
+//     console.log(a+b);
+// }
+// addNumbers(3,7)
+
+// ----------------------------- function expressionda -----------------------------------------
+// const addNumbers = function (a, b) {
+//   console.log(a + b);
+// };
+// addNumbers(3, 7);
+
+// ----------------------------- arrow functionda ----------------------------------------------
+// const addNumbers = (a, b) => {
+//   console.log(a + b);
+// }
+// addNumbers(3,7)
+
+// 3-misol
+
+// ----------------------------- function declaration ------------------------------------------
+// function greet(name) {
+//   alert(`Salom, ${name}`);
+// }
+// let name=prompt("Ismingizni kiriting: ")
+// greet(name)
+
+// ----------------------------- function expressionda -----------------------------------------
+// const greet = function (name) {
+//   alert(`Salom, ${name}`);
+// }
+
+// let name=prompt("Ismingizni kiriting: ")
+// greet(name)
+
+// ----------------------------- arrow functionda ----------------------------------------------
+// const greet = (name) => {
+//   alert(`Salom, ${name}`);
+// };
+// let name = prompt("Ismingizni kiriting: ");
+// greet(name);
+
+// 4-misol
+
+// ----------------------------- function declaration ------------------------------------------
+// function fullName(firstName, lastName){
+//     alert(`${firstName} ${lastName}`)
+// }
+
+// let firstName=prompt("Ismingizni kiriting: ")
+// let lastName=prompt("Familyangizni kiriting: ")
+// fullName(firstName , lastName)
+
+// ----------------------------- function expressionda -----------------------------------------
+// const fullName=function(firstName, lastName){
+//     alert(`${firstName} ${lastName}`)
+// }
+
+// let firstName=prompt("Ismingizni kiriting: ")
+// let lastName=prompt("Familyangizni kiriting: ")
+// fullName(firstName , lastName)
+
+// ----------------------------- arrow functionda ----------------------------------------------
+// const fullName=(firstName, lastName)=>{
+//     alert(`${firstName} ${lastName}`)
+// }
+
+// let firstName=prompt("Ismingizni kiriting: ")
+// let lastName=prompt("Familyangizni kiriting: ")
+// fullName(firstName , lastName)
+
+// ----------------------------- function declaration ------------------------------------------
+// function rectanglePerimeter(length, width){
+//     alert(`${(length+width)*2}`)
+// }
+// let length = +prompt("To'rtburchak uzunligini kiriting: ")
+// let width= +prompt("To'rtburchak kengligini kiriting: ")
+// rectanglePerimeter(length,width)
+
+// ----------------------------- function expressionda -----------------------------------------
+// const rectanglePerimeter=function(length, width){
+//     alert(`${(length+width)*2}`)
+// }
+// let length = +prompt("To'rtburchak uzunligini kiriting: ")
+// let width= +prompt("To'rtburchak kengligini kiriting: ")
+// rectanglePerimeter(length,width)
+
+// ----------------------------- arrow functionda ----------------------------------------------
+// const rectanglePerimeter=(length, width)=>{
+//     alert(`${(length+width)*2}`)
+// }
+// let length = +prompt("To'rtburchak uzunligini kiriting: ")
+// let width= +prompt("To'rtburchak kengligini kiriting: ")
+// rectanglePerimeter(length,width)
+
+// 6-misol
+
+// ----------------------------- function declaration ------------------------------------------
+// function triangleSides(a, b, c) {
+//   console.log(`Tomon a: ${a}`);
+//   console.log(`Tomon b: ${b}`);
+//   console.log(`Tomon c: ${c}`);
+// }
+// triangleSides(3,4,5)
+
+// ----------------------------- function expressionda -----------------------------------------
+// const triangleSides = function(a, b, c) {
+//   console.log(`Tomon a: ${a}`);
+//   console.log(`Tomon b: ${b}`);
+//   console.log(`Tomon c: ${c}`);
+// }
+// triangleSides(3,4,5)
+
+// ----------------------------- arrow functionda ----------------------------------------------
+// const triangleSides =(a, b, c)=> {
+//   console.log(`Tomon a: ${a}`);
+//   console.log(`Tomon b: ${b}`);
+//   console.log(`Tomon c: ${c}`);
+// }
+// triangleSides(3,4,5)
+
+// 7-misol
+
+// ----------------------------- function declaration ------------------------------------------
+// function squareNumber(number){
+//     alert(Math.pow(number, 2))
+// }
+// let number = +prompt("Son kiriting: ")
+
+// squareNumber(number)
+
+// ----------------------------- function expressionda -----------------------------------------
+// const squareNumber = function(number){
+//     alert(Math.pow(number, 2))
+// }
+// let number = +prompt("Son kiriting: ")
+
+// squareNumber(number)
+
+// ----------------------------- arrow functionda ----------------------------------------------
+// const squareNumber = (number) => {
+//     alert(Math.pow(number, 2))
+// }
+// let number = +prompt("Son kiriting: ")
+
+// squareNumber(number)
+
+// 8-misol
+
+// ----------------------------- function declaration ------------------------------------------
+// function animalInfo(name, age){
+//     console.log(`Bu ${name} ${age} yoshda`);
+// }
+// animalInfo("Mushuk", 2)
+
+// ----------------------------- function expressionda -----------------------------------------
+// const animalInfo = function(name, age){
+//     console.log(`Bu ${name} ${age} yoshda`);
+// }
+// animalInfo("Mushuk", 2)
+
+// ----------------------------- arrow functionda ----------------------------------------------
+// const animalInfo = (name, age) => {
+//     console.log(`Bu ${name} ${age} yoshda`);
+// }
+// animalInfo("Mushuk", 2)
+
+// 9-misol
+
+// ----------------------------- function declaration ------------------------------------------
+// function sayHello() {
+//   console.log("Salom Dunyo!");
+// }
+// sayHello()
+
+// ----------------------------- function expressionda -----------------------------------------
+// const sayHello = function() {
+//   console.log("Salom Dunyo!");
+// }
+// sayHello()
+
+// ----------------------------- arrow functionda ----------------------------------------------
+// const sayHello = () =>  {
+//   console.log("Salom Dunyo!");
+// }
+// sayHello()
+
+// 10-misol
+
+// ----------------------------- function declaration ------------------------------------------
+// function printNmaes(name1, name2, name3, name4, name5) {
+//   console.log(name1);
+//   console.log(name2);
+//   console.log(name3);
+//   console.log(name4);
+//   console.log(name5);
+// }
+
+// printNmaes("Ali", "Vali", "Gulbahor", "Zilola", "Javohir")
+
+// ----------------------------- function expressionda -----------------------------------------
+// const printNmaes = function(name1, name2, name3, name4, name5) {
+//   console.log(name1);
+//   console.log(name2);
+//   console.log(name3);
+//   console.log(name4);
+//   console.log(name5);
+// }
+
+// printNmaes("Ali", "Vali", "Gulbahor", "Zilola", "Javohir")
+
+// ----------------------------- arrow functionda ----------------------------------------------
+// const printNmaes = (name1, name2, name3, name4, name5) => {
+//   console.log(name1);
+//   console.log(name2);
+//   console.log(name3);
+//   console.log(name4);
+//   console.log(name5);
+// }
+
+// printNmaes("Ali", "Vali", "Gulbahor", "Zilola", "Javohir")
